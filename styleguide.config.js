4d3e7bcc6ae8fe3b5adaa249310a96e1b6d40ac5
Sara Vieira <hey@iamsaravieira.com>
2017-10-16 16:12:36 +0100

    Add topoly

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,17 +1,18 @@
+const webpackConfig = require('react-scripts/config/webpack.config.dev.js');
+const { defaultHandlers } = require('react-docgen');
+const dnHandler = require('react-docgen-displayname-handler');
+const path = require('path');
+
+
 module.exports = {
   components: 'src/index.js',
   defaultExample: true,
   title: 'React Topology',
   showUsage: true,
   showSidebar: false,
-  webpackConfig: {
-    module: {
+  webpackConfig: Object.assign(webpackConfig, {
+    module: Object.assign(webpackConfig.module, {
       rules: [
-        {
-          test: /\.jsx?$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader'
-        },
         {
           test: /\.svg$/,
           loader: 'svg-inline-loader'
@@ -21,14 +22,28 @@ module.exports = {
           use: ['style-loader', 'css-loader']
         },
         {
+          test: /\.(js|jsx)$/,
+          use: ['babel-loader']
+        },
+        {
           test: /\.(eot|ttf|woff|woff2)$/,
           use: [
             {
               loader: 'file-loader'
             }
           ]
+        },
+        {
+          test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
+          loader: 'url-loader',
+          options: {
+            limit: 10000,
+            name: 'static/media/[name].[hash:8].[ext]'
+          }
         }
       ]
-    }
-  }
+    })
+  }),
+  handlers: componentPath =>
+    defaultHandlers.concat(dnHandler.createDisplayNameHandler(componentPath))
 };
